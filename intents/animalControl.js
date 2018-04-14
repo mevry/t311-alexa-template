71a142cdf45866b92212e316a149e202da96d2fc
Derek Miller <derekjmiller@gmail.com>
2018-04-14 08:51:04 -0700

    changes to animalControl

--- intents/animalControl.js
+++ intents/animalControl.js
@@ -10,39 +10,37 @@ exports.animalControlIntents = {
         }
         const residentCity = intent.slots.residentCity.value;
         
-        if(!intent.slots.contactType.value){
-            const elicitSlot = 'contactType';
-            const speechOutputEnd = 'If you would like, I can send you their contact information, call them for you, or both.';
-            const repromptSpeech = 'Would you like me to send you their contact information, call them for you, or both?';
-
+        if(!intent.slots.contactConfirm.value){
+            const elicitSlot = 'contactConfirm';
+            const speechOutputEnd = 'Would you like me to send you their contact information?';
+            const repromptSpeech = speechOutputEnd;
+            let speechOutput = '';
             switch(residentCity.toLowerCase()){           
                 case "tacoma":
-                    const speechOutput = `You can contact Tacoma Animal Care and Control. Their number is (253) 627-PETS. ${speechOutputEnd}`;
+                    speechOutput = `You can contact Tacoma Animal Care and Control. Their number is (253) 627-7387. ${speechOutputEnd}`;
                     this.emit(':elicitSlot',elicitSlot,speechOutput,repromptSpeech);
                     break;
                 case "fircrest":
-                    this.response.speak("Call a doctor its fircrest.");
+                    speechOutput = `You can contact Fircrest Police. Their number is (253) 565-1198. ${speechOutputEnd}`;
+                    this.emit(':elicitSlot',elicitSlot,speechOutput,repromptSpeech);
                     break;
                 default:
                     this.response.speak("I don't know that area");
             }
-            this.emit('responseReady');
+            this.emit(':responseReady');
         }
-        const contactType = intent.slots.contactType.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+        const contactConfirm = intent.slots.contactConfirm.resolutions.resolutionsPerAuthority[0].values[0].value.name;
         
-        switch(contactType){
-            case 'send':
+        switch(contactConfirm){
+            case 'yes':
                 this.response.speak("Email sent.");
                 break;
-            case 'call':
-                this.response.speak("Dialing...");
-                break;
-            case 'both':
-                this.response.speak("Email sent. Now dialing...");
-                
+            case 'no':
+                this.response.speak("You said no");
+                break; 
             default:
                 this.response.speak(`Contact type is: ${contactType}`);
-            break;
+                break;
         }
         this.emit(':responseReady');
         
